382bf086c116926e29e402814456f7806dae104b
Kepler1205 <owensellen@gmail.com>
2023-05-12 20:46:13 +0100

    use ~/.fehbg by default, or scroll through wallpaper dir

--- i3/scripts/grayscale-background.sh
+++ i3/scripts/grayscale-background.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-feh --no-fehbg --bg-fill ~/pictures/wallpapers/skull-jap.png
+
+if [ -f ~/.fehbg ]; then
+    ~/.fehbg
+else
+    pgrep $HOME/.config/i3/scripts/grayscale-background.sh && exit
+    WALLS_DIR="$HOME/pictures/wallpapers/grayscale/active"
+    while true; do
+        feh --no-fehbg --bg-fill "$WALLS_DIR/$(ls $WALLS_DIR | sort -R | tail -1)"
+        sleep 10m
+    done
+fi
