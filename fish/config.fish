3825dfa87a6cd9f1e31b0c8ba75c2d06c30a73a2
Kepler1205 <owensellen@gmail.com>
2023-05-20 01:27:57 +0100

    abbrs involving nvim use $EDITOR env var

--- fish/config.fish
+++ fish/config.fish
@@ -46,23 +46,32 @@ function last_history_item
   echo $history[1]
 end
 
+function new_term
+    if test $argv
+        $TERMINAL --working-directory (pwd) -e $argv &; disown
+    else 
+        $TERMINAL --working-directory (pwd) &; disown
+    end
+end
+
 #### Abbrieviations and aliases ####
 
 # Aliases
-alias cp='cp -iv'
-alias mv='mv -iv'
-alias df='df -h'
-alias free='free -m'
-alias ls='exa --icons -s type'
-alias la='exa --icons -s type -a'
-alias ll='exa --icons -s type -la --no-time --no-filesize'
-alias lll='exa --icons -s type -lahgmU --classify'
-alias llll='exa --icons -s type -laahHiUumg@ --changed --classify'
-alias tree='exa -T --header --icons'
-alias c='clear'
+alias c    'clear'
+alias l    'exa --icons -s type -l --no-time --no-filesize'
+alias ls   'exa --icons -s type'
+alias la   'exa --icons -s type -a'
+alias ll   'exa --icons -s type -la --no-time --no-filesize'
+alias lll  'exa --icons -s type -lahgmU --classify'
+alias llll 'exa --icons -s type -laahHiUumg@ --changed --classify'
+alias tree 'exa --icons --header -T'
 
 # Programs
-abbr --position anywhere v 'nvim'
+abbr cp 'cp -iv'
+abbr mv 'mv -iv'
+abbr df 'df -h'
+abbr --set-cursor cd 'cd % ;ls'
+abbr --position anywhere v '$EDITOR'
 abbr nf 'neofetch'
 abbr cm 'unimatrix -s 96'
 abbr q 'qalc'
@@ -74,7 +83,8 @@ abbr ln 'ln -s'
 abbr t 'tldr'
 abbr d 'doas'
 abbr sudo 'doas'
-abbr ns 'alacritty --working-directory $(pwd) & disown'
+abbr nt 'new_term'
+abbr ntv 'new_term $EDITOR'
 abbr feh 'feh --no-fehbg'
 abbr --set-cursor e 'echo -e "%"'
 abbr --set-cursor find "find . -iname '*%*'"
@@ -84,9 +94,9 @@ abbr --set-cursor timer 'sleep %m && notify-send --urgency=critical --wait "Time
 # Git abbrieviations
 abbr gs 'git status'
 abbr ga 'git add'
-abbr --set-cursor gc 'git commit -m "%"'
 abbr gp 'git push'
 abbr gd 'git diff'
+abbr --set-cursor gc 'git commit -m "%"'
 
 # Program modifiers
 abbr --set-cursor --position anywhere sed "sed 's/%/g'"
@@ -108,15 +118,15 @@ abbr --position anywhere romdir '/mnt/hdd/ROMs/'
 abbr --position anywhere hdd '/mnt/hdd/'
 
 # Vim Abbrieviations
-abbr vi3 'nvim -O ~/.config/i3/config ~/.config/i3/themes/$THEME.conf ~/.config/i3/keybinds.conf' 
-abbr vnv 'cd ~/.config/nvim; nvim lua/custom/chadrc.lua'
-abbr vpo 'nvim -O ~/.config/polybar/themes/$THEME/*.ini'
-abbr vpi 'nvim -O ~/.config/picom/picom.conf'
-abbr vfi 'nvim ~/.config/fish/config.fish'
-abbr vdu 'nvim ~/.config/dunst/dunstrc'
-abbr vro 'nvim ~/.config/rofi/$THEME.rasi'
-abbr vco 'nvim ~/.config/conky/conky.conf'
-abbr val 'nvim ~/.config/alacritty/$THEME.yml'
+abbr vi3 '$EDITOR -O ~/.config/i3/config ~/.config/i3/themes/$THEME.conf ~/.config/i3/keybinds.conf' 
+abbr vnv 'cd ~/.config/nvim; $EDITOR lua/custom/chadrc.lua'
+abbr vpo '$EDITOR -O ~/.config/polybar/themes/$THEME/*.ini'
+abbr vpi '$EDITOR -O ~/.config/picom/picom.conf'
+abbr vfi '$EDITOR ~/.config/fish/config.fish'
+abbr vdu '$EDITOR ~/.config/dunst/dunstrc'
+abbr vro '$EDITOR ~/.config/rofi/$THEME.rasi'
+abbr vco '$EDITOR ~/.config/conky/conky.conf'
+abbr val '$EDITOR ~/.config/alacritty/$THEME.yml'
 
 # Startup ascii
 if test "$TERM" = "alacritty"
