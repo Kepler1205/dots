28caf57963ab02f9cee05c5179a60cbd9524f34d
Kepler1205 <owensellen@gmail.com>
2023-05-26 16:07:06 +0100

    offline documentation rofi script

---
+++ i3/scripts/rofi/documentation.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+# rofi script to run localy stored websites
+# intended to be used for opening documentation
+#
+# download documentation with
+# wget --mirror --page-requisites --convert-links --no-clobber --no-parent --no-verbose <site-link>
+# if <download-dir>/index.html does not exist, symlink that path to the index.html
+#
+#### NOTE:                                      ####
+#### there must be an "index.html" or symlink   ####
+#### to one in the root of the website dir      ####
+#### e.g: "~/documentation/i3wm.org/index.html" ####
+
+doc_path=$HOME/documentation
+[ -d $doc_path ] || exit
+
+err() {
+    notify-send --expire-time 2500 --urgency critical "ERROR: $1"
+    exit 1
+}
+
+sel=$(ls $doc_path | rofi -dmenu)
+
+[ -z "$sel" ] && err "No selection"
+[ -e "$doc_path/$sel/index.html" ] || err "$doc_path/$sel/index.html does not exist"
+
+[ $BROWSER = "firefox" ] && firefox --new-window $doc_path/$sel/index.html; exit 0
