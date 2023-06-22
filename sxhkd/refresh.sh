633b617f9c6889b5c355c468bcb52f5a95022b95
Kepler1205 <owensellen@gmail.com>
2023-06-22 10:05:42 +0100

    sxhkd can now use multiple files with refresh.sh

---
+++ sxhkd/refresh.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+#
+# refresh sxhkd 
+#
+
+
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    echo "XDG_CONFIG_HOME is not set, defaulting to ~/.config/sxhkd/"
+    sxhkd_path="~/.config/sxhkd/"
+else
+    sxhkd_path="$XDG_CONFIG_HOME/sxhkd"
+fi
+
+cd $sxhkd_path
+
+killall sxhkd
+cat bindings/* > .sxhkdrc
+setsid sxhkd -c $sxhkd_path/.sxhkdrc > /dev/null 2>&1 &
