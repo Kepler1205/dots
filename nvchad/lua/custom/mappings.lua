8692d16e373de47adb8bff5bcd22991e5a3e2d10
Kepler1205 <owensellen@gmail.com>
2023-07-06 19:40:38 +0100

    removed J and K for moving lines

--- nvchad/lua/custom/mappings.lua
+++ nvchad/lua/custom/mappings.lua
@@ -34,8 +34,8 @@ vim.keymap.set("n", "<C-u", "<C-u>zz", opts)
 -- insert mode:
 
 -- visual mode:
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 
 return M
