41ff7aa06dad0df35ca973c3cd39c973f0f107c9
Kepler1205 <kepler02@proton.me>
2024-01-18 10:39:05 +0000

    remove unused extract function

--- fish/config.fish
+++ fish/config.fish
@@ -57,44 +57,6 @@ function new_term
     end
 end
 
-function ex
-    if test -f $argv
-        switch $argv
-            case *.tar.bz2
-				tar xjf $argv   
-            case *.tar.gz
-				tar xzf $argv   
-            case *.bz2
-				bunzip2 $argv   
-            case *.rar
-				unrar x $argv   
-            case *.gz
-				gunzip $argv    
-            case *.tar
-				tar xf $argv    
-            case *.tbz2
-				tar xjf $argv   
-            case *.tgz
-				tar xzf $argv   
-            case *.zip
-				unzip $argv     
-            case *.Z
-				uncompress $argv
-            case *.7z
-				7z x $argv      
-            case *.deb
-				ar x $argv      
-            case *.tar.xz
-				tar xf $argv    
-            case *.tar.zst
-				unzstd $argv    
-            case '*'
-				echo $argv cannot be extracted via ex()
-        end
-        echo $argv is not a valid file 
-    end
-end
-
 # opens files in specified program
 # kind of like xdg-open
 function o
