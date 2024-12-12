b2b2a74322e02d7771f17eca8e82050b66c6e851
Kepler1205 <kepler02@proton.me>
2024-12-12 23:54:49 +0000

    use mpv with bindings and control mpv with polybar

--- polybar/scripts/get_spotify_status.sh
+++ polybar/scripts/get_spotify_status.sh
@@ -10,7 +10,7 @@ PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 # Use `playerctld` to always detect the latest player.
 # See more here: https://github.com/altdesktop/playerctl/#selecting-players-to-control
 
-PLAYER="spotify,ncspot"
+PLAYER="mpv,spotify,ncspot"
 
 # Format of the information displayed
 # Eg. {{ artist }} - {{ album }} - {{ title }}
